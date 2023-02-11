49e94378b31353c00a916ef7b9c862f738669896
Romildo Marques <git@romildo.me>
2023-02-11 20:45:48 -0300

    Update env-config.sh

--- env-config.sh
+++ env-config.sh
@@ -27,6 +27,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+# nvm list-remote
 nvm install v18.14.0
 node -v
 echo "Instalando dependencias frontend..."
